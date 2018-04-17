11d75734755b69d00717e6573b384cacf5f92852
Chafik El idrissi <chafik.elidrissi@outlook.fr>
2018-04-17 16:45:43 +0200

    Version test sans fonction JS qui touche le DOM

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div id="App">
     <h2>Insertion dynamique</h2>
-    <button @click="addChild()">Ajouter Colonne</button>
+    <button v-on:click="nbPane++, setResizer=!setResizer">
+      Ajouter Colonne
+    </button>  NB Colonnes : {{ this.nbPane }}
     <multipane
       class="custom-resizer"
+      v-on:click="addChild()"
       layout="vertical"
       ref="container"
     >
+      <div v-for="n in nbPane">
+        <pane v-on:addChild="addChild"/>
+        <multipane-resizer v-if="setResizer"/>
+      </div>
     </multipane>
   </div>
 </template>
@@ -14,10 +21,9 @@
 <script>
 import swal from 'sweetalert';
 import { Multipane, MultipaneResizer } from 'vue-multipane';
-import Vue from 'vue';
+// import Vue from 'vue';
 import Pane from './components/Pane';
 import Button from './components/Button';
-
 
 export default {
   name: 'App',
@@ -27,45 +33,63 @@ export default {
     Pane,
     Button,
   },
+  data() {
+    return {
+      nbPane: 0,
+      setResizer: true,
+    };
+  },
   methods: {
-    addChild(id) {
-      swal('I Making Child');
-      let identity;
-      if (id !== undefined) {
-        identity = 0;
-      } else {
-        identity = document.getElementsByClassName('custom-resizer');
+    addChild() {
+      // eslint-disable-next-line
+      console.log("-> ", this.nbPane % 2);
+      if (this.nbPane % 2 === 0) {
+        swal('1');
+        this.setResizer = true;
       }
+      this.nbPane += 1;
 
-      const ComponentClass = Vue.extend(Pane);
-      const ComponentClassRes = Vue.extend(MultipaneResizer);
-      const instance = new ComponentClass({
-        propsData: { type: 'primary' },
-      });
-      const instanceRes = new ComponentClassRes({
-        propsData: { type: 'primary' },
-      });
-
-      // eslint-disable-next-line
-      // console.log(this.$refs);
-      instance.$mount(); // pass nothing
-      instanceRes.$mount();
-
-      // eslint-disable-next-line
-      console.log("This -> ", this);
-      // eslint-disable-next-line
-      console.log("Instance ",identity[0]);
-      // eslint-disable-next-line
-      console.log("ID -> ",identity[0].childElementCount);
-      if (identity[0].childElementCount !== 0) {
-        identity[0].appendChild(instanceRes.$el);
-      }
-      identity[0].appendChild(instance.$el);
+      swal('This Is Your Child !');
+      // const identity = document.getElementsByClassName('custom-resizer');
+      //
+      // const ComponentClass = Vue.extend(Pane);
+      // const ComponentClassRes = Vue.extend(MultipaneResizer);
+      // const instance = new ComponentClass({
+      //   propsData: { type: 'primary' },
+      // });
+      // const instanceRes = new ComponentClassRes({
+      //   propsData: { type: 'primary' },
+      // });
+      //
+      // // eslint-disable-next-line
+      // // console.log(this.$refs);
+      // instance.$mount(); // pass nothing
+      // instanceRes.$mount();
+      //
+      //
+      // // eslint-disable-next-line
+      // console.log("Instance ",instance);
+      // // // eslint-disable-next-line
+      // // console.log("ID -> ",identity[0].childElementCount);
+      // if (identity[0].childElementCount !== 0) {
+      //   identity[0].appendChild(instanceRes.$el);
+      // }
+      // identity[0].setAttribute('v-on:addChild', 'this.addChild');
+      // identity[0].appendChild(instance.$el);
+      //
+      // // eslint-disable-next-line
+      // console.log("Test -> ", identity[0]);
+      // // eslint-disable-next-line
     },
   },
 };
 </script>
 <style scoped>
+  #app {
+    width: 100%;
+    height: 100%;
+  }
+
   .custom-resizer {
     width: 100%;
     height: 400px;
--- src/components/Pane.vue
+++ src/components/Pane.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="pane"
-       @click="addChild"
-       :style="{ minWidth: '10%', maxWidth: '90%'}">
+       @click="addChild()"
+       :style="{ minWidth: '10%', maxWidth: '90%', flexGrow:1}">
+    <slot/>
   </div>
 </template>
 <script>
 /* eslint-disable no-underscore-dangle */
 
 import { Multipane, MultipaneResizer } from 'vue-multipane';
+import swal from 'sweetalert';
 
 export default{
   name: 'pane',
@@ -17,15 +19,17 @@ export default{
   },
   methods: {
     addChild() {
-      let id;
+      swal('I Want A Child !');
+      let uid;
       if (this._uid === 3) {
-        id = 0;
+        uid = 0;
       } else {
-        id = this._uid - 4;
+        uid = this._uid - 4;
       }
+      const identity = document.getElementById(uid);
       // eslint-disable-next-line
-      console.log("Child ID -> ", id);
-      this.$emit('addChild', id);
+      console.log("Child ID -> ", this);
+      this.$emit('addChild', identity);
     },
   },
 };
@@ -42,20 +46,44 @@ export default{
   }
   .layout-v > .multipane-resizer {
     height: 40px;
+    width: 4px;
+    margin: 0;
+    display: block;
+    left: 2px;
+    margin-top: 45%;
+    border-left: 2px solid #ffb100;
   }
   .layout-h > .multipane-resizer {
     height: 40px;
-  }
-  .multipane-resizer {
     margin: 0;
     display: block;
     width: 5px;
-    height: 40px;
     left: 0px;
     margin-top: 175px;
     margin-left: 5px;
     margin-right: 5px;
     border-left: 1px solid #ccc;
-    border-right: 1px solid #ccc;
   }
 </style>
+<!--
+<div class="custom-resizer" layout="vertical" class=""pane>
+  &lt;!&ndash; Instancier  par programme &ndash;&gt;
+  <pane class="pane"
+        v-for="(tree, index) in nodes"
+        :key="index"
+        :nodes="tree.nodes"
+        :label="tree.label"
+        :asnodes="tree.asnodes"
+        :asresizer="tree.asresizer"
+        :slot="resizer"
+  />
+  <div v-if=!this.asnodes> {{ this.label }} </div>
+  <div v-if="!this.asresizer">
+    <slot/>
+  </div>
+  <slot name="resizer">
+    {{ this.label }}
+    <multipane-resizer/>
+  </slot>
+</div>
+-->
